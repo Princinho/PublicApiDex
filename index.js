94910908373aeea4904e4ac6b5c9162fbdd57832
Princinho <princegnakou@gmail.com>
2022-09-21 09:20:20 +0000

    Fix deployement and layout issues

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/* 
-    Bonus Challenge 
-
-    Fetch the list of 642 open APIs from
-        https://api.publicapis.org/entries
-        
-    Create a my-api component
-        display the name and category of the API,
-        the description, and also display the type 
-        of Auth (if any) and whether or not the API 
-        supports HTTPS
-    
-    Use CSS Grid to style my-api
-        The title and category should be 
-        listed as Title (Category) 
-        and should link to the API docs
-        
-    The grid should have 4 rows
-        3rem, 1rem, 4rem, 3rem respectively
-        and 3 columns each 1/3rd of available width
-        
-    Finally, display all of the APIs
-*/
 
 async function getAPIs() {
     let response=await fetch('https://api.publicapis.org/entries')
@@ -30,11 +7,12 @@ async function getAPIs() {
 }
 
 function getAPIhtml(myAPI) {
-    console.log(myAPI)
-    return `<div class='my-api'>
+    // console.log(myAPI)
+    return `    
+            <div class='my-api'>
                 <h2 class='api-name'>
-                    ${myAPI.API}
-                    <a class='api-link' href='${myAPI.Link}'>(${myAPI.Category})</a>
+                    
+                    <a class='api-link' href='${myAPI.Link}'> ${myAPI.API}(${myAPI.Category})</a>
                 </h2>
                 
                 <p class='api-description'>${myAPI.Description}</p>
@@ -44,7 +22,9 @@ function getAPIhtml(myAPI) {
 }
 
 function displayAPIs(myAPIs) {
-    document.body.innerHTML=myAPIs.map(api=>getAPIhtml(api)).join('')
+    document.body.innerHTML=`
+    <h1>The list of all public API</h1>
+    <div class="main-container">${myAPIs.map(api=>getAPIhtml(api)).join('')}</div>`
 }
 
 getAPIs()
